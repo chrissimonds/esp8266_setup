9a0cefa548d28811e8da75061fbefc6aa6785207
Chris Simonds <32778284+chrissimonds@users.noreply.github.com>
2025-01-18 19:09:23 -0600

    Create webrepl_cfp.py
    Initial commit of webrepl config file.
    Please see: https://docs.micropython.org/en/latest/esp8266/quickref.html#webrepl-web-browser-interactive-prompt

---
+++ webrepl_cfp.py
@@ -0,0 +1,7 @@
+# This file and password is created the import webrepl_setup is issued for configuration
+# See https://docs.micropython.org/en/latest/esp8266/quickref.html#webrepl-web-browser-interactive-prompt
+# There is an assumption that this process has already been completed in this code
+# import webrepl_setup
+
+PASS = '1234'
+# Please note that this is a sample password made during initial setup so please choose something more secure
