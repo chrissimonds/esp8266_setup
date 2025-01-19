d4e877d25ec02763d1e9ef6544fcd7925ea1a96b
Chris Simonds <32778284+chrissimonds@users.noreply.github.com>
2025-01-18 19:58:44 -0600

    Update webrepl_cfp.py
    punctuation update

--- webrepl_cfp.py
+++ webrepl_cfp.py
@@ -1,6 +1,6 @@
 # This file and password is created the import webrepl_setup is issued for configuration
 # See https://docs.micropython.org/en/latest/esp8266/quickref.html#webrepl-web-browser-interactive-prompt
-# There is an assumption that this process has already been completed in this code
+# There is an assumption that this process has already been completed in this code:
 # import webrepl_setup
 
 PASS = '1234'
